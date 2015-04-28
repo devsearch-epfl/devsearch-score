80bef653469859c70e3e1f633f09a46ae51ae73c
julien graisse <julien.graisse@epfl.ch>
2015-04-28 15:14:14 +0200

    debugging the score function

--- src/main/scala-2.10/FileScorer.scala
+++ src/main/scala-2.10/FileScorer.scala
@@ -1,13 +1,22 @@
 import scala.collection.immutable.ListMap
 
+
 /**
  * Created by julien on 24.04.15.
  * Still a test. For now, I will try to implement a function computing
  * a code file's score based on given attributes weights
  */
+
 object FileScorer {
-  def score(searchOutput : Map<CodeFileData, List[Int]): CodeFileData = {
-    val temp = searchOutput mapValues (x => x zip weights map (elem => elem._1*elem._2) foldleft(0) (_+_) )
+
+  val weights = (1,2,3)
+
+  /**
+   * Input: A mapping between a file and the list of the values of all its superfeatures.
+   * Output: A mapping between a file and its score, the Map is sorted by decreasing score
+   */
+  def score(searchOutput : Map<CodeFileData, List[Int]): ListMap<CodeFileData, List[Int] = {
+    val temp = searchOutput mapValues (x => x zip weights map (elem => elem._1*elem._2)) mapValues (x => x.foldLeft(0)(_+_))
     ListMap(temp.toSeq.sortWith(_._2 > _._2):_*)
 
   }
